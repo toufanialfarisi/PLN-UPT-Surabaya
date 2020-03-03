54ae3a82e439633bab1ec25df1c1c8cf1195979d
toufani1515 <toufani1515@gmail.com>
2020-03-03 23:55:22 +0800

    penambahan script automasi

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,5 @@
 heroku config:set APP_SETTINGS=config.Development
 heroku config:set FLASK_ENV=production
 heroku config:set SECRET_KEY=uptsurabayaterbaik2020
+heroku run sh migrate.sh
+heroku run sh migrate.sh
